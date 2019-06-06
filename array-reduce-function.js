c9f35dd3083f1318f7ebda62f32025b554058532
Satish Kumar <satish.kumar@kelltontech.com>
2019-06-06 09:56:26 +0530

    array reduce function

---
+++ array-reduce-function.js
@@ -0,0 +1,9 @@
+const numbers = [1, -1, 2, 3];
+let sum =0;
+for (let n of numbers) {
+    sum += n;
+}
+console.log(sum);
+numbers.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+});
